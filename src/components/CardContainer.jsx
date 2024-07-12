d4ae517acf95b243e0a7c03c020a10d3a8f52248
rngnnn <rngnnnclk@gmail.com>
2024-07-12 13:40:00 +0200

    state

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -1,38 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import {data} from "../helper/data"
-import { Col, Container, Row } from 'react-bootstrap'
+import { Col, Container, Form, Row } from 'react-bootstrap'
 import Player from './Player'
 
 const CardContainer = () => {
 // console.log(data);
+const[arama,setArama]=useState("")
 
+//!1.yol
+ 
+// let filtrelenmişData= data.filter((a)=>a.name.includes(arama))
+ // console.log(filtrelenmişData);
   return (
     <>
-{/* input açılacak */}
+      <Form.Control className='w-50 m-auto' type="search" placeholder="Search Player..." onChange={(e)=> setArama(e.target.value)} />
 
-<Container>
-<Row>
-{data.map((player)=>(
-<Col key={player.name}>
-
-   <Player{...player}/>
-
-
-</Col>
-
-))}
-
-</Row>
-
-
-</Container>
-
-
-
-
+      <Container className="kart-container rounded-4 my-4 p-3 ">
+        <Row className="g-3 ">
+          {/* {filtrelenmişData.map((player) => ( */}
+          {data.filter((abc)=>abc.name.toLowerCase().includes(arama.toLowerCase())).map((player) => (
+            <Col md={6} lg={4} xl={3} key={player.name}>
+              <Player {...player} />
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </>
-  )
+  );
 }
 
 export default CardContainer
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,20 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Card } from 'react-bootstrap'
 
 const Player = ({name,img,statistics}) => {
-    const [showResim,setShow]=useState(true)
 
+    const[showResim,setShow]=useState(true)
    
   return (
-    <Card className='rounded-2 m-auto player-kart' role="button">
-      <Card.Img className='player-resim' variant="top" src={img} />
+    <Card
+      className="rounded-2 m-auto player-kart"
+      role="button"
+      onClick={() => setShow(!showResim)}
+    >
+      {showResim ? (
+        <Card.Img className="player-resim" variant="top" src={img} />
+      ) : (
+        <ul className='m-auto'>
+         
+{statistics.map((item,index)=>(
+    <li key={index} className='h5 list-unstyled text-start'>
+    
+    
+    🏀{item}</li>
+))}
 
-      <Card.Footer >
- <Card.Title>
-    {name}
- </Card.Title>
-       
+        </ul>
+      )}
 
+      <Card.Footer>
+        <Card.Title>{name}</Card.Title>
       </Card.Footer>
     </Card>
   );
